4780678a1ef9d9c9e3a92c85a16fc34998734b90
Ryan Hope <rmh3093@gmail.com>
2012-09-23 10:09:36 -0400

    Add hooks
    Move pygame specific code out of the actr package. Test environment
    listens for events from python package and acts appropriately

---
+++ examples/jni/actr6_jni/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding:    utf-8 -*-
+#===============================================================================
+# This file is part of ACTR6_JNI.
+# Copyright (C) 2012 Ryan Hope <rmh3093@gmail.com>
+#
+# ACTR6_JNI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ACTR6_JNI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ACTR6_JNI.  If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
+
+from chunks import VisualChunk
+from server import JNI_Server
+from dispatcher import Dispatcher
+
+__version__ = '1.0.0'
