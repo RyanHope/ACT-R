5e5bad78414d93ffb699f961ba0deb0eec1373cc
Ryan Hope <rmh3093@gmail.com>
2013-02-17 16:33:10 -0500

    fix helper script

--- update-asdf-source-registry.lisp
+++ update-asdf-source-registry.lisp
@@ -1,3 +1,3 @@
-(ensure-directories-exist "~/.config/common-lisp/source-registry.conf")
+(ensure-directories-exist "~/.config/common-lisp/source-registry.conf/")
 (with-open-file (s "~/.config/common-lisp/source-registry.conf/10-actr6.conf" :direction :output :if-exists :supersede)
   (format s "(:tree ~S)~%" (directory-namestring (or *compile-file-truename* *load-truename*))))
